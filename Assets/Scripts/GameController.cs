3582c662d0d64f837d5b4c6430d72623a57c5cb3
Tarmo Jussila <tarmo.jussila@gmail.com>
2018-01-26 21:32:15 +0200

    WIP: Game controller. Controls game states.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Game states.
+/// </summary>
+public enum GameState
+{
+    Menu,   // Menu.
+    Intro,  // Intro (start cutscene).
+    Game,   // Game.
+    Outro   // Outro (end cutscene).
+}
+
+/// <summary>
+/// Game controller. Controls game states.
+/// </summary>
+public class GameController : MonoBehaviour
+{
+    public static GameController Instance { get; private set; }
+
+    public GameState CurrentGameState;
+
+    // Awake.
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start.
+    private void Start()
+    {
+        CurrentGameState = GameState.Menu;
+    }
+
+    // Change game state.
+    private void ChangeGameState(GameState gameState)
+    {
+        CurrentGameState = gameState;
+    }
+}
