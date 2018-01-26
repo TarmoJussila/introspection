9951f97aa13633a9319e520a572dab6a2f729e0d
Aleksi Toivonen <aletoivonen@gmail.com>
2018-01-26 21:05:50 +0200

    Added stuff to files

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public string HorizontalAxis;
+	public string VerticalAxis;
+
+	public float MoveSpeed;
+
+	private Vector2 movementVector;
+
+	private Rigidbody rb;
+	private Collider collider;
+
+	private bool InAtmosphere;
+
+	// Use this for initialization
+	void Start () {
+	
+		rb = GetComponent<Rigidbody> ();
+		collider = GetComponent<Collider> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		movementVector.x = Input.GetAxisRaw (HorizontalAxis);
+		movementVector.y = Input.GetAxisRaw (VerticalAxis);
+
+	}
+
+	void FixedUpdate () {
+
+
+
+
+	}
+}
