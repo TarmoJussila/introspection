fbb659a3e87406a482ea04c9139fe5b1b5c216c8
Tarmo Jussila <tarmo.jussila@gmail.com>
2018-01-27 17:44:22 +0200

    Objective indicator is better.

--- Assets/Scripts/ObjectiveController.cs
+++ Assets/Scripts/ObjectiveController.cs
@@ -1,6 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
+
+/// <summary>
+/// Objective indicator state.
+/// </summary>
+[System.Serializable]
+public class ObjectiveIndicator
+{
+    [Range(0, 200)]
+    public float IndicatorDistance;
+    [Range(0f, 1f)]
+    public float GrainIntensity;
+    [Range(0, 10)]
+    public float AnimatorSpeed;
+}
 
 /// <summary>
 /// Objective controller.
@@ -10,15 +25,20 @@ public class ObjectiveController : MonoBehaviour
     public static ObjectiveController Instance { get; private set; }
 
     public int ObjectivePointAmount = 5;
+    
+    public ObjectiveIndicator LowIndicator;
+    public ObjectiveIndicator MediumIndicator;
+    public ObjectiveIndicator HighIndicator;
+
+    public ObjectiveIndicator DefaultIndicator;
 
     public float DistanceCheckWaitTime = 1.0f;
     public float InitialDistanceCheckWaitTime = 3.0f;
 
-    public float LowIndicatorDistance = 75f;
-    public float MediumIndicatorDistance = 30f;
-    public float HighIndicatorDistance = 10f;
-
     public List<GameObject> Objectives = new List<GameObject>();
+
+    [Header("Post Processing")]
+    public PostProcessingProfile PostProcessingProfile;
 
     // Awake.
     private void Awake()
@@ -29,6 +49,11 @@ public class ObjectiveController : MonoBehaviour
     // Start.
     private void Start()
 	{
+        // Initial grain settings.
+        var grainSettings = PostProcessingProfile.grain.settings;
+        grainSettings.intensity = DefaultIndicator.GrainIntensity;
+        PostProcessingProfile.grain.settings = grainSettings;
+
         StartCoroutine(InitialWaitTime());
 	}
 
@@ -57,26 +82,34 @@ public class ObjectiveController : MonoBehaviour
             }
         }
 
-        if (closestDistance < HighIndicatorDistance)
+        var grainSettings = PostProcessingProfile.grain.settings;
+
+        if (closestDistance < HighIndicator.IndicatorDistance)
         {
-            float playbackSpeed = ObjectiveHandler.Instance.AnimatorPlaybackSpeedMax;
+            float playbackSpeed = HighIndicator.AnimatorSpeed;
             ObjectiveHandler.Instance.SetIndicatorSpeed(playbackSpeed);
+            grainSettings.intensity = HighIndicator.GrainIntensity;
         }
-        else if (closestDistance < MediumIndicatorDistance)
+        else if (closestDistance < MediumIndicator.IndicatorDistance)
         {
-            float playbackSpeed = (ObjectiveHandler.Instance.AnimatorPlaybackSpeedMin + ObjectiveHandler.Instance.AnimatorPlaybackSpeedMax) / 2f;
+            float playbackSpeed = MediumIndicator.AnimatorSpeed;
             ObjectiveHandler.Instance.SetIndicatorSpeed(playbackSpeed);
+            grainSettings.intensity = MediumIndicator.GrainIntensity;
         }
-        else if (closestDistance < LowIndicatorDistance)
+        else if (closestDistance < LowIndicator.IndicatorDistance)
         {
-            float playbackSpeed = ObjectiveHandler.Instance.AnimatorPlaybackSpeedMin * 2f;
+            float playbackSpeed = LowIndicator.AnimatorSpeed;
             ObjectiveHandler.Instance.SetIndicatorSpeed(playbackSpeed);
+            grainSettings.intensity = LowIndicator.GrainIntensity;
         }
         else
         {
-            float playbackSpeed = ObjectiveHandler.Instance.AnimatorPlaybackSpeedMin;
+            float playbackSpeed = DefaultIndicator.AnimatorSpeed;
             ObjectiveHandler.Instance.SetIndicatorSpeed(playbackSpeed);
+            grainSettings.intensity = DefaultIndicator.GrainIntensity;
         }
+
+        PostProcessingProfile.grain.settings = grainSettings;
 
         Debug.Log("Closest distance: " + closestDistance);
 
