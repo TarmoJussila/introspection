aadf2be992a6f488f51684b9725e59ee4caad0ab
Aleksi Toivonen <aletoivonen@gmail.com>
2018-01-27 22:30:59 +0200

    New antenna and random asteroid targets

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+
+    public GameObject DeathParticles;
+
+    void OnCollisionEnter (Collision coll) {
+
+        GameObject g = (GameObject)Instantiate(DeathParticles, coll.contacts[0].point, Quaternion.identity);
+        Destroy(gameObject);
+
+    }
+}
