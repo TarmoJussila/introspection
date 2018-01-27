01a5323ec3f7b9d1d4b741fc29d4ee693c0ff67e
Tarmo Jussila <tarmo.jussila@gmail.com>
2018-01-27 15:19:10 +0200

    Added skybox controller. Selects a random skybox material from list.

---
+++ Assets/Scripts/SkyboxController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Skybox controller. Sets random skybox.
+/// </summary>
+public class SkyboxController : MonoBehaviour
+{
+    public List<Material> Skyboxes = new List<Material>();
+
+	// Start.
+	private void Start()
+	{
+        RenderSettings.skybox = Skyboxes[Random.Range(0, Skyboxes.Count)];
+
+        DynamicGI.UpdateEnvironment();
+    }
+}
