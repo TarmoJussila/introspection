1dc8289c6eeab72f2d3a71fc7873bb8251f9b6bf
Aleksi Toivonen <aletoivonen@gmail.com>
2018-01-27 19:41:43 +0200

    player stuff

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,68 +5,91 @@ using UnityEngine;
 /// <summary>
 /// Player.
 /// </summary>
-public class Player : MonoBehaviour
-{
-    public static Player Instance { get; private set; }
+public class Player : MonoBehaviour {
+	public static Player Instance { get; private set; }
 
-    public string HorizontalAxis;
-    public string VerticalAxis;
-    public string RotateAxis;
+	public string HorizontalAxis;
+	public string VerticalAxis;
+	public string RotateAxis;
 
-    public float MoveSpeed;
-    public float RotateSpeed;
-    public float FloatHeight;
+	public float MoveSpeed;
+	public float RotateSpeed;
+	public float FloatHeight;
 
-    private Vector2 movementVector;
-    private Vector3 groundNormal;
+	private Vector2 movementVector;
+	private Vector3 groundNormal;
 
-    public Transform CurrentPlanet;
-    public bool InAtmosphere;
+	public Transform CurrentPlanet;
+	public bool InAtmosphere;
 
-    private bool grounded;
+	public Transform LeftRaycast;
+	public Transform RightRaycast;
 
-    // Awake.
-    private void Awake()
-    {
-        Instance = this;
-    }
+	private bool grounded;
 
-    // Start.
-    private void Start()
-    {
-    }
+	// Awake.
+	private void Awake () {
+		Instance = this;
+	}
 
-    // Update.
-    private void Update()
-    {
-        movementVector.x = Input.GetAxisRaw(HorizontalAxis);
-        movementVector.y = Input.GetAxisRaw(VerticalAxis);
+	// Start.
+	private void Start () {
+	}
 
-        FloatHeight = Mathf.Sin(Time.time) + 3;
-    }
+	// Update.
+	private void Update () {
+		movementVector.x = Input.GetAxisRaw (HorizontalAxis);
+		movementVector.y = Input.GetAxisRaw (VerticalAxis);
 
-    // Fixed update.
-    private void FixedUpdate()
-    {
-        RaycastHit hit = new RaycastHit();
-        if (Physics.Raycast(transform.position, CurrentPlanet.position - transform.position, out hit))
-        {
-            groundNormal = (transform.position - hit.point).normalized;
-        }
+		FloatHeight = Mathf.Sin (Time.time) + 3;
+	}
 
-		Quaternion rot = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-		transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, 0.1f);
-		transform.Rotate(new Vector3(0, Input.GetAxisRaw(RotateAxis) * RotateSpeed, 0));
+	// Fixed update.
+	private void FixedUpdate () {
+		RaycastHit hit = new RaycastHit ();
+		if (Physics.Raycast (transform.position, CurrentPlanet.position - transform.position, out hit)) {
+			groundNormal = (transform.position - hit.point).normalized;
+		}
 
-        Vector3 targetPos = hit.point + (groundNormal * FloatHeight);
+		Quaternion rot = Quaternion.FromToRotation (transform.up, hit.normal) * transform.rotation;
+		transform.rotation = Quaternion.RotateTowards (transform.rotation, rot, 0.1f);
+		transform.Rotate (new Vector3 (0, Input.GetAxisRaw (RotateAxis) * RotateSpeed, 0));
+
+		Vector3 targetPos = hit.point + (groundNormal * FloatHeight);
+
+		hit = new RaycastHit ();
         
-		//if (Physics.Raycast(transform.position, 
+		if (Physics.Raycast (LeftRaycast.position, transform.forward, out hit, 3)) {
+			Debug.DrawLine (transform.position, hit.point);
+		} else if (Physics.Raycast (RightRaycast.position, transform.forward, out hit, 3)) {
+			Debug.DrawLine (transform.position, hit.point);
+		} else if (movementVector.y > 0) {
+			targetPos += transform.forward * movementVector.y;
+		}
 
-		targetPos += transform.forward * movementVector.y;
-		targetPos += transform.right * movementVector.x;
+		if (Physics.Raycast (transform.position, -transform.forward, out hit, 3)) {
+			Debug.DrawLine (transform.position, hit.point);
+		} else if (movementVector.y < 0) {
+			targetPos += transform.forward * movementVector.y;
+		}
 
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, MoveSpeed);
+		if (Physics.Raycast (transform.position, transform.right, out hit, 3)) {
+			Debug.DrawLine (transform.position, hit.point);
+		} else if (movementVector.x > 0) {
+			targetPos += transform.right * movementVector.x;
+		}
+
+		if (Physics.Raycast (transform.position, -transform.right, out hit, 3)) {
+			Debug.DrawLine (transform.position, hit.point);
+		} else if (movementVector.x < 0) {
+			targetPos += transform.right * movementVector.x;
+		}
+
+
+		//targetPos += transform.right * movementVector.x;
+
+		transform.position = Vector3.MoveTowards (transform.position, targetPos, MoveSpeed);
 
         
-    }
+	}
 }
