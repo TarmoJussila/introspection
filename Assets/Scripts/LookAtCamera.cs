c7ef65c07abe2346e8bd8e0de77db6b80a554d6a
Tarmo Jussila <tarmo.jussila@gmail.com>
2018-01-28 07:44:27 +0200

    First asteroid time prolonged. WIP: Final objective light beam. Camera far clipping plane 175.

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// LookAtCamera.
+/// </summary>
+public class LookAtCamera : MonoBehaviour
+{
+	
+	// Update.
+	private void Update()
+	{
+        transform.LookAt(new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z), transform.position);
+	}
+}
